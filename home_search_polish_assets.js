36605f0f06c8a10cc0defbc969c07e22743d147b
mikealien56 <mike.santana421@gmail.com>
2026-09-14 17:29:40 -0300

    Publish simplified home and search UI

---
+++ home_search_polish_assets.js
@@ -0,0 +1,15 @@
+const fs=require('fs');
+const path=require('path');
+const pub=path.join(__dirname,'public');
+const src=path.join(__dirname,'home_search_polish.js');
+const dst=path.join(pub,'home_search_polish.js');
+if(fs.existsSync(src)){
+  fs.mkdirSync(pub,{recursive:true});
+  fs.copyFileSync(src,dst);
+}
+const idx=path.join(pub,'index.html');
+if(fs.existsSync(idx)){
+  let h=fs.readFileSync(idx,'utf8');
+  if(!h.includes('/home_search_polish.js')) h=h.replace('</body>','<script src="/home_search_polish.js?v=1"></script>\n</body>');
+  fs.writeFileSync(idx,h);
+}
