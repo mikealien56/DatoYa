4a26244977dfd4be8c5f05a55e600385d2f65dcf
mikealien56 <mike.santana421@gmail.com>
2026-09-17 11:00:52 -0300

    Load marketplace navigation fix after legacy UI

--- local_market_home_assets.js
+++ local_market_home_assets.js
@@ -1,7 +1,7 @@
 const fs=require('fs'),path=require('path');
 const root=__dirname,pub=path.join(root,'public');
 fs.mkdirSync(pub,{recursive:true});
-for(const name of ['local_market_home.css','local_market_home.js','local_market_home_guard.js','local_location_ui.js']){
+for(const name of ['local_market_home.css','local_market_home.js','local_market_home_guard.js','local_location_ui.js','marketplace_navigation_fix.js']){
   const src=path.join(root,name),dst=path.join(pub,name);
   if(fs.existsSync(src))fs.copyFileSync(src,dst);
 }
@@ -17,8 +17,9 @@ if(fs.existsSync(idx)){
   html=html.replace(/<script[^>]+src="\/local_market_home\.js[^\"]*"[^>]*><\/script>\s*/g,'');
   html=html.replace(/<script[^>]+src="\/local_market_home_guard\.js[^\"]*"[^>]*><\/script>\s*/g,'');
   html=html.replace(/<script[^>]+src="\/local_location_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
+  html=html.replace(/<script[^>]+src="\/marketplace_navigation_fix\.js[^\"]*"[^>]*><\/script>\s*/g,'');
 
   html=html.replace('</head>','<link rel="stylesheet" href="/local_market_home.css?v=20260917-3">\n</head>');
-  html=html.replace('</body>','<script src="/local_market_home.js?v=20260917-3"></script>\n<script src="/local_market_home_guard.js?v=20260917-4"></script>\n<script src="/local_location_ui.js?v=20260917-4"></script>\n</body>');
+  html=html.replace('</body>','<script src="/local_market_home.js?v=20260917-3"></script>\n<script src="/local_market_home_guard.js?v=20260917-4"></script>\n<script src="/local_location_ui.js?v=20260917-4"></script>\n<script src="/marketplace_navigation_fix.js?v=20260917-1"></script>\n</body>');
   fs.writeFileSync(idx,html);
 }
