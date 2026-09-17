c44681fbd56f1d3cb207bee8e17d60b1bb5e14cc
mikealien56 <mike.santana421@gmail.com>
2026-09-17 13:17:10 -0300

    Load Mercado Pago commerce UI

--- marketplace_commerce_assets.js
+++ marketplace_commerce_assets.js
@@ -1,6 +1,6 @@
 const fs=require('fs'),path=require('path');
 const root=__dirname,pub=path.join(root,'public');fs.mkdirSync(pub,{recursive:true});
-for(const name of ['marketplace_commerce.css','marketplace_commerce_ui.js','marketplace_impulse_home.js','marketplace_commerce_admin_ui.js']){
+for(const name of ['marketplace_commerce.css','marketplace_commerce_ui.js','marketplace_impulse_home.js','marketplace_commerce_admin_ui.js','marketplace_payments_ui.js']){
   const src=path.join(root,name),dst=path.join(pub,name);if(fs.existsSync(src))fs.copyFileSync(src,dst);
 }
 const idx=path.join(pub,'index.html');
@@ -10,7 +10,8 @@ if(fs.existsSync(idx)){
   html=html.replace(/<script[^>]+src="\/marketplace_commerce_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
   html=html.replace(/<script[^>]+src="\/marketplace_impulse_home\.js[^\"]*"[^>]*><\/script>\s*/g,'');
   html=html.replace(/<script[^>]+src="\/marketplace_commerce_admin_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
-  html=html.replace('</head>','<link rel="stylesheet" href="/marketplace_commerce.css?v=20260917-1">\n</head>');
-  html=html.replace('</body>','<script src="/marketplace_commerce_ui.js?v=20260917-1"></script>\n<script src="/marketplace_impulse_home.js?v=20260917-1"></script>\n<script src="/marketplace_commerce_admin_ui.js?v=20260917-1"></script>\n</body>');
+  html=html.replace(/<script[^>]+src="\/marketplace_payments_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
+  html=html.replace('</head>','<link rel="stylesheet" href="/marketplace_commerce.css?v=20260917-2">\n</head>');
+  html=html.replace('</body>','<script src="/marketplace_commerce_ui.js?v=20260917-1"></script>\n<script src="/marketplace_impulse_home.js?v=20260917-1"></script>\n<script src="/marketplace_commerce_admin_ui.js?v=20260917-1"></script>\n<script src="/marketplace_payments_ui.js?v=20260917-1"></script>\n</body>');
   fs.writeFileSync(idx,html);
 }
