b72e43c3c87c9076e5152622f5a7c2965d6b9b69
mikealien56 <mike.santana421@gmail.com>
2026-09-17 07:46:32 -0300

    Complete territorial GPS phase

--- local_market_home_assets.js
+++ local_market_home_assets.js
@@ -20,6 +20,6 @@ if(fs.existsSync(idx)){
   html=html.replace(/<script[^>]+src="\/business_market_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
 
   html=html.replace('</head>','<link rel="stylesheet" href="/local_market_home.css?v=20260917-3">\n</head>');
-  html=html.replace('</body>','<script src="/local_market_home.js?v=20260917-3"></script>\n<script src="/local_market_home_guard.js?v=20260917-3"></script>\n<script src="/local_location_ui.js?v=20260917-3"></script>\n<script src="/business_market_ui.js?v=20260917-1"></script>\n</body>');
+  html=html.replace('</body>','<script src="/local_market_home.js?v=20260917-3"></script>\n<script src="/local_market_home_guard.js?v=20260917-3"></script>\n<script src="/local_location_ui.js?v=20260917-4"></script>\n<script src="/business_market_ui.js?v=20260917-1"></script>\n</body>');
   fs.writeFileSync(idx,html);
 }
