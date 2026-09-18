cde2b44c0c57a21ea87eae69a80d1092104b74ba
mikealien56 <mike.santana421@gmail.com>
2026-09-17 21:59:31 -0300

    Strip legacy worker and job UI from public marketplace shell

--- marketplace_public_shell_assets.js
+++ marketplace_public_shell_assets.js
@@ -9,7 +9,7 @@ if(fs.existsSync(idx)){
   let html=fs.readFileSync(idx,'utf8');
   html=html.replace(/<script[^>]+src="\/marketplace_legacy_route_guard\.js[^\"]*"[^>]*><\/script>\s*/g,'');
   // Estas capas pertenecen únicamente a la antigua experiencia de maestros/trabajos.
-  for(const name of ['chat_ui_fix','home_request_fix','worker_own_profile_ui','role_ui_fix','worker_v2_ui','protection_ui','gps_ui','workflow_v2_ui','gps_map_ui']){
+  for(const name of ['chat_ui_fix','home_request_fix','worker_own_profile_ui','nearby_ui','search_ui_fix','favorites_ui','role_ui_fix','admin_v2_ui','admin_core_ui_fix','admin_operations_ui','admin_v3_fix','worker_v2_ui','worker_profile_fix','worker_portfolio_ui','worker_finance_ui','request_photos_ui','protection_ui','gps_ui','workflow_v2_ui','gps_map_ui','gps_map_ui_v2','evidence_ui','review_ui','reports_ui','verification_admin_ui','verification_worker_ui','request_target_ui','direct_worker_category_fix','request_wizard_ui','job_finish_guard_ui','request_detail_ui_fix','job_detail_ui','job_flow_ui_bridge','worker_profile_polish_ui','worker_specialties_ui','worker_portfolio_ui','worker_onboarding_ui','professional_account_hub_ui','onboarding_flow_ui','admin_disputes_ui']){
     const re=new RegExp('<script[^>]+src="\\/'+name.replace(/[.*+?^${}()|[\]\\]/g,'\\$&')+'\\.js[^\"]*"[^>]*><\\/script>\\s*','g');
     html=html.replace(re,'');
   }
