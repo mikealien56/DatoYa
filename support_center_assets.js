fb170f9259534544ffd347cc0af235b513d1f499
mikealien56 <mike.santana421@gmail.com>
2026-09-21 13:30:38 -0400

    Bump support center asset version

--- support_center_assets.js
+++ support_center_assets.js
@@ -5,6 +5,6 @@ const idx=path.join(pub,'index.html');
 if(fs.existsSync(idx)){let html=fs.readFileSync(idx,'utf8');
 html=html.replace(/<link[^>]+href="\/support_center\.css[^"]*"[^>]*>\s*/g,'');
 html=html.replace(/<script[^>]+src="\/support_center_ui\.js[^"]*"[^>]*><\/script>\s*/g,'');
-html=html.replace('</head>','<link rel="stylesheet" href="/support_center.css?v=20260921-1">\n</head>');
-html=html.replace('</body>','<script src="/support_center_ui.js?v=20260921-1"></script>\n</body>');
+html=html.replace('</head>','<link rel="stylesheet" href="/support_center.css?v=20260921-2">\n</head>');
+html=html.replace('</body>','<script src="/support_center_ui.js?v=20260921-2"></script>\n</body>');
 fs.writeFileSync(idx,html);}
