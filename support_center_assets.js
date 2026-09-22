0e9723f1805920650541e386d0c90b38cd3fb919
mikealien56 <mike.santana421@gmail.com>
2026-09-22 13:09:03 -0400

    Publish business support UI assets

--- support_center_assets.js
+++ support_center_assets.js
@@ -1,10 +1,11 @@
 const fs=require('fs'),path=require('path');
 const root=__dirname,pub=path.join(root,'public');fs.mkdirSync(pub,{recursive:true});
-for(const name of ['support_center_ui.js','support_center.css']){const s=path.join(root,name),d=path.join(pub,name);if(fs.existsSync(s))fs.copyFileSync(s,d);}
+for(const name of ['support_center_ui.js','business_support_ui.js','support_center.css']){const s=path.join(root,name),d=path.join(pub,name);if(fs.existsSync(s))fs.copyFileSync(s,d);}
 const idx=path.join(pub,'index.html');
 if(fs.existsSync(idx)){let html=fs.readFileSync(idx,'utf8');
 html=html.replace(/<link[^>]+href="\/support_center\.css[^"]*"[^>]*>\s*/g,'');
 html=html.replace(/<script[^>]+src="\/support_center_ui\.js[^"]*"[^>]*><\/script>\s*/g,'');
-html=html.replace('</head>','<link rel="stylesheet" href="/support_center.css?v=20260921-2">\n</head>');
-html=html.replace('</body>','<script src="/support_center_ui.js?v=20260921-2"></script>\n</body>');
+html=html.replace(/<script[^>]+src="\/business_support_ui\.js[^"]*"[^>]*><\/script>\s*/g,'');
+html=html.replace('</head>','<link rel="stylesheet" href="/support_center.css?v=20260922-1">\n</head>');
+html=html.replace('</body>','<script src="/support_center_ui.js?v=20260922-1"></script>\n<script src="/business_support_ui.js?v=20260922-1"></script>\n</body>');
 fs.writeFileSync(idx,html);}
