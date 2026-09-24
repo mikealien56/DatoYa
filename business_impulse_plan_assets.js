d807145b1773c81a5a8705859d79bd66655349bc
mikealien56 <mike.santana421@gmail.com>
2026-09-23 20:23:48 -0400

    Bust fixed Impulso plan cache

--- business_impulse_plan_assets.js
+++ business_impulse_plan_assets.js
@@ -4,6 +4,6 @@ for(const name of ['business_impulse_plan_ui.js','business_impulse_plan.css']){c
 const idx=path.join(pub,'index.html');
 if(fs.existsSync(idx)){let h=fs.readFileSync(idx,'utf8');
 h=h.replace(/<link[^>]+href="\/business_impulse_plan\.css[^"]*"[^>]*>\s*/g,'').replace(/<script[^>]+src="\/business_impulse_plan_ui\.js[^"]*"[^>]*><\/script>\s*/g,'');
-h=h.replace('</head>','<link rel="stylesheet" href="/business_impulse_plan.css?v=20260922-4">\n</head>');
-h=h.replace('</body>','<script src="/business_impulse_plan_ui.js?v=20260923-1"></script>\n</body>');
+h=h.replace('</head>','<link rel="stylesheet" href="/business_impulse_plan.css?v=20260923-5">\n</head>');
+h=h.replace('</body>','<script src="/business_impulse_plan_ui.js?v=20260923-2"></script>\n</body>');
 fs.writeFileSync(idx,h);}
