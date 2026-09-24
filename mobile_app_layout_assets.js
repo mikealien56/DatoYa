00773fcd6c5f9ed417c9ed24ab1e093af3e262cb
mikealien56 <mike.santana421@gmail.com>
2026-09-23 21:00:55 -0400

    Bust clean mobile layout cache

--- mobile_app_layout_assets.js
+++ mobile_app_layout_assets.js
@@ -7,6 +7,6 @@ fs.copyFileSync(path.join(__dirname,cssName),path.join(publicDir,cssName));
 const indexPath=path.join(publicDir,'index.html');
 let html=fs.readFileSync(indexPath,'utf8');
 html=html.replace(/<link[^>]+href="\/mobile_app_layout\.css[^"]*"[^>]*>\s*/g,'');
-html=html.replace('</head>','  <link rel="stylesheet" href="/mobile_app_layout.css?v=20260923-1">\n</head>');
+html=html.replace('</head>','  <link rel="stylesheet" href="/mobile_app_layout.css?v=20260924-2">\n</head>');
 fs.writeFileSync(indexPath,html);
 console.log('[DatoYa] Capa responsive PWA final preparada.');
