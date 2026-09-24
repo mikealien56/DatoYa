cb0d7046dcd96bf1dcfa16a6809466d93af8ce46
mikealien56 <mike.santana421@gmail.com>
2026-09-23 20:32:11 -0400

    Bust unified account flow cache

--- marketplace_account_assets.js
+++ marketplace_account_assets.js
@@ -10,7 +10,7 @@ if(fs.existsSync(idx)){
   let html=fs.readFileSync(idx,'utf8');
   html=html.replace(/<link[^>]+href="\/marketplace_account\.css[^\"]*"[^>]*>\s*/g,'');
   html=html.replace(/<script[^>]+src="\/marketplace_account_ui\.js[^\"]*"[^>]*><\/script>\s*/g,'');
-  html=html.replace('</head>','<link rel="stylesheet" href="/marketplace_account.css?v=20260917-1">\n</head>');
-  html=html.replace('</body>','<script src="/marketplace_account_ui.js?v=20260922-3"></script>\n</body>');
+  html=html.replace('</head>','<link rel="stylesheet" href="/marketplace_account.css?v=20260923-2">\n</head>');
+  html=html.replace('</body>','<script src="/marketplace_account_ui.js?v=20260923-4"></script>\n</body>');
   fs.writeFileSync(idx,html);
 }
